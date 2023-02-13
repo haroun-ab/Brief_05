782817a322f6045bd37882e03d73aadfbdc63c73
Haroun <hrnabd@hotmail.com>
2023-02-13 09:09:46 +0100

    adding, edition and deleting notes implemented

--- front/js/liste.js
+++ front/js/liste.js
@@ -32,7 +32,6 @@ xhr.onreadystatechange = function() {
         for(let i = 0; i < studentTab.length; i++){
           if(studentArray[i].nomPrenom == studentTab[i].querySelector('span').innerText){
             const annualMoy = (trimestreAvg(studentArray[i].t1) + trimestreAvg(studentArray[i].t2) + trimestreAvg(studentArray[i].t3)) / 3
-
             studentTab[i].querySelector('.moyGen').innerText = annualMoy.toFixed(2) // arrondi au dixième
           }
         }
@@ -42,54 +41,32 @@ xhr.onreadystatechange = function() {
 
 // Fonction qui calcule la moyenne d'un seul trimestre 
 function trimestreAvg(trimestre){
-  let moyfrancaisT1 = 0
-  let coefSumFr = 0
 
-  let moyMathT1 = 0
-  let coefSumMath = 0
+  let moyenneTrimestre = 0
 
-  let moyAnglaisT1 = 0
-  let coefSumAng = 0
+ 
 
-  let moyHistGeoT1 = 0
-  let coefSumHg = 0
-
-  let moyEpsT1 = 0
-  let coefSumEps = 0   
-  const noteTab = trimestre.notation.francais.notes
-
+  const matiereTab = ['francais', 'math', 'anglais', 'histGeo', 'eps']
+    // Boucle pour parcourir toutes les matières
+  for(let i = 0; i<matiereTab.length; i++){
+    let moySum = 0
+    let coefSum = 0   
+    const noteTab = trimestre.notation[matiereTab[i]].notes
   // Boucle pour parcourir toutes les notes d'une seule matière
-  for (let x = 0; x < noteTab.length; x++){
-    // Moyenne Français
-    coefSumFr += trimestre.notation.francais.notes[x][1]
-    moyMathT1 += trimestre.notation.francais.notes[x][0] * trimestre.notation.francais.notes[x][1] 
-  
-    // Moyenne Math
-    coefSumMath += trimestre.notation.math.notes[x][1]
-    moyfrancaisT1 += trimestre.notation.math.notes[x][0] * trimestre.notation.math.notes[x][1] 
-  
-    // Moyenne Anglais
-    coefSumAng += trimestre.notation.anglais.notes[x][1]
-    moyAnglaisT1 += trimestre.notation.anglais.notes[x][0] * trimestre.notation.anglais.notes[x][1] 
-
-    // Moyenne Histoire-Géo
-    coefSumHg += trimestre.notation.histGeo.notes[x][1]
-    moyHistGeoT1 += trimestre.notation.histGeo.notes[x][0] * trimestre.notation.histGeo.notes[x][1] 
-
-    // Moyenne EPS
-    coefSumEps += trimestre.notation.eps.notes[x][1]
-    moyEpsT1 += trimestre.notation.eps.notes[x][0] * trimestre.notation.eps.notes[x][1] 
+    for (let x = 0; x < noteTab.length; x++){
+  coefSum += noteTab[x][1]
+  moySum += noteTab[x][0] * noteTab[x][1]
+}
+    moyenneTrimestre += (moySum/coefSum) * trimestre.notation[matiereTab[i]].coef
   }
+  moyenneTrimestre/=13
 
-  const moyenneTrimestre = (moyfrancaisT1/ coefSumFr *2 + moyMathT1/ coefSumMath *2 + moyHistGeoT1/ coefSumHg *5 + moyAnglaisT1/ coefSumAng *1 + moyEpsT1/ coefSumEps * 3) / (1+2+2+3+5)
+
+
+  
   // Je retourne la moyenne
   return moyenneTrimestre
 }
-
-
-
-
-
 
 // Déconnexion
 document.querySelector('a#logout').onclick = () => {
