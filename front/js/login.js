2a272c0c531e7cefa7086a92833224dbeae1dad3
rania.chikhaoui.simplon@gmail.com <rania.chikhaoui.simplon@gmail.com>
2023-02-13 09:18:00 +0100

    Verification login

--- front/js/login.js
+++ front/js/login.js
@@ -1,51 +1,53 @@
-if (sessionStorage.getItem('session') && sessionStorage.getItem('session')== 'prof'){
-  location.pathname = '/front/html/liste.html'
+if (
+  sessionStorage.getItem("session") &&
+  sessionStorage.getItem("session") == "prof"
+) {
+  location.pathname = "/front/html/liste.html";
 }
 
-if (sessionStorage.getItem('session') && sessionStorage.getItem('session')== 'eleves'){
-  location.pathname = '/front/html/notes.html'
+if (
+  sessionStorage.getItem("session") &&
+  sessionStorage.getItem("session") == "eleves"
+) {
+  location.pathname = "/front/html/notes.html";
 }
-const form = document.querySelector('form')
+const form = document.querySelector("form");
 
 form.onsubmit = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const dataObj = {
-        pseudo: document.querySelector('#id').value,
-        mdp: document.querySelector('#pw').value
+  const dataObj = {
+    pseudo: document.querySelector("#id").value,
+    mdp: document.querySelector("#pw").value,
+  };
+  // Création et configuration d'un objet de requête AJAX
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://127.0.0.1:8000/login.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xhr.setRequestHeader("Access-Control-Allow-Methods", "*");
+
+  // Code à exécuter une fois le fichier JSON est chargé*
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Récupération de la réponse contenant les données JSON
+      const data = this.response;
+
+      if (data === "teacher successful log in") {
+        location = "./liste.html";
+        sessionStorage.setItem("session", "prof");
+      }
+
+      if (data === "student successful log in") {
+        location = `./notes.html?id=${dataObj.pseudo}`;
+        sessionStorage.setItem("session", "eleves");
+      }
+
+      console.log(sessionStorage.getItem("session"));
+
+      // Manipulation du DOM pour récupérer aux données XML
+      // et les afficher dans les cards
     }
-    // Création et configuration d'un objet de requête AJAX
-const xhr = new XMLHttpRequest();
-xhr.open("POST", "http://127.0.0.1:8000/login.php", true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-xhr.setRequestHeader("Access-Control-Allow-Methods", "*");
-
- // Code à exécuter une fois le fichier JSON est chargé*
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    // Récupération de la réponse contenant les données JSON
-    const data = this.response;
-    
-    
-if(data === "teacher successful log in"){
-    location = "./liste.html"
-    sessionStorage.setItem('session', 'prof');
-}
-
-if(data === "student successful log in"){
-  location = `./notes.html?id=${dataObj.pseudo}`
-  sessionStorage.setItem('session', 'eleves');
-}
-
-
-console.log(sessionStorage.getItem('session'))
-
-    // Manipulation du DOM pour récupérer aux données XML
-    // et les afficher dans les cards
-  }
-}
-xhr.send(`data= ${JSON.stringify(dataObj)}`);
-}
-
-
+  };
+  xhr.send(`data= ${JSON.stringify(dataObj)}`);
+};
